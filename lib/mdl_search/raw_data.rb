e236599a1b305509e8eba440089ebc24f298d809
Ryu Ishikawa <ryu.ishikawa@howtelevision.jp>
2016-08-13 22:07:21 +0900

    fix

--- lib/mdl_search/raw_data.rb
+++ lib/mdl_search/raw_data.rb
@@ -1,6 +1,6 @@
 module MdlSearch
   class << self
-    RAW_GROUPS_TABLE = YAML.load(File.read(File.expand_path('./yaml/groups.yml')))
-    RAW_ICONS_TABLE = YAML.load(File.read(File.expand_path('./yaml/icons.yml')))
+    RAW_GROUPS_TABLE = YAML.load(File.read(File.expand_path('../../../yaml/groups.yml', __FILE__)))
+    RAW_ICONS_TABLE = YAML.load(File.read(File.expand_path('../../../yaml/icons.yml', __FILE__)))
   end
 end
